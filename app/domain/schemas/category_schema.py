e0a330929b70aa1e66697d27cdb30b972fe6d62b
Juan Esteban <valdezjuanes014@gmail.com>
2024-11-25 16:37:23 -0500

    Nuevo deploy en railway, nueva coneccion al servidor de pg y se añadio descripcion a las categorias

--- app/domain/schemas/category_schema.py
+++ app/domain/schemas/category_schema.py
@@ -4,11 +4,13 @@ from typing import Optional, List
 class CategoryCreate(BaseModel):
     name: str
     parent_id: Optional[int] = None # Se puede especificar el padre al crear una categoría
+    description: Optional[str] = None
 
 class CategoryResponse(BaseModel):
     id_category: int
     name: str
     parent_id: Optional[int] = None # Muestra el id del padre si existe
+    description: Optional[str] = None
     subcategories: List["CategoryResponse"] = Field(default_factory=list) # Recursivamente muestra subcategorías
 
     class Config:
